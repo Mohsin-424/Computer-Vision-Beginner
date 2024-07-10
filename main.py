76742bf7eb68c6a9bd34139e627c6a88a41f7c14
mi873345 <mi873345@gmail.com>
2024-07-10 09:50:23 +0500

    Biceps and OrthoSynergy Angles and Poses

---
+++ main.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import cvzone
+
+cap = cv2.VideoCapture("bicep.mp4")
+
+
+
+while True:
+    rt,frame=cap.read()
+    frame = cv2.resize(frame,(600,480))
+    
+    
+    cv2.imshow('frame',frame)
+    cv2.waitKey(1)
+    
