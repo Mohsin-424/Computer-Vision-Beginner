94af2225d46dc538dea611731ebbaf9ab4fb7a1b
Muhammad Mohsin <mohsinpu24@gmail.com>
2024-08-02 21:44:16 +0500

    QR-READER

--- QR-Reader.py
+++ QR-Reader.py
@@ -2,8 +2,25 @@
 
 import numpy as np
 import cv2
-import pyzbar
+import pyzbar # library to read qr codes and bar codes
 import matplotlib as plt
+
+import os 
+from pyzbar.pyzbar import decode
 
 # Steps for QR Reader
 
+input_dir = '/G:\Computer Vision\PortFolio/img-1.jpeg'
+
+for j in sorted( os.listdir ( input_dir )):
+    # Imread always reads image....
+    img = cv2.imread( os.path.join( input_dir ,j ))
+    
+    qr_info = decode(img)
+    
+    print(qr_info)
+    
+    
+    
+    
+    
---
+++ qr-reader-attendance-system-master/README.md
@@ -0,0 +1,5 @@
+# qr-reader-attendance-system
+
+QR reader + attendance system with Python and OpenCV !
+
+[![Watch the video](https://img.youtube.com/vi/5eahBYvDJjA/0.jpg)](https://www.youtube.com/watch?v=5eahBYvDJjA)
